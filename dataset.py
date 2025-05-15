980a5e441fa0c35bbbebb5c1c2d75d9dbd94aea8
Yuxuama <matthieulecomte@free.fr>
2025-05-15 10:17:20 +0200

    Working version of Dataset and DatasetGroup structure

--- dataset.py
+++ dataset.py
@@ -3,12 +3,11 @@ Implement the dataset structure
 """
 from graph import Network
 from networkx import from_numpy_array, betweenness_centrality
-from analysis import compute_xhi
+from analysis import measure_etas_from_xhi, measure_individual_asymmetry
 import numpy as np
 
 def selector_parser(selector):
-    """Parse selector
-    Selector format specification: must be key or list of key"""
+    """Parse selector for more selection freedom (useful ?)"""
     pass
 
 class Dataset:
@@ -19,120 +18,239 @@ class Dataset:
         self.data = {}
     
     def init_with_network(self, net: Network):
-        
+        """Init Dataset with all local values of the network"""
         self.size = net.parameters["Community size"]
         vertices = net.vertices
         link_adjacency = net.get_link_adjacency_matrix()
+        trust_adjacency = net.get_trust_adjacency_matrix()
         kxgraph_link = from_numpy_array(link_adjacency)
         centralities = betweenness_centrality(kxgraph_link)
         out_degrees = np.sum(link_adjacency, axis=1)
         in_degrees = np.sum(link_adjacency, axis=0)
+        asymmetries = measure_individual_asymmetry(link_adjacency, full=True)
         for i in range(self.size):
             i_data = {}
+            # Graph index
+            index = vertices[i].index
             # Phenotype
             i_data["Phenotype"] = vertices[i].phenotype
             # Load
             i_data["Load"] = vertices[i].load / net.parameters["Cognitive capacity"]
             # Outdegree
-            i_data["Out degree"] = out_degrees[i]
+            i_data["Out degree"] = out_degrees[index]
             # Indegree
-            i_data["In degree"] = in_degrees[i]
+            i_data["In degree"] = in_degrees[index]
             # Centrality
-            i_data["Centrality"] = centralities[i]
+            i_data["Centrality"] = centralities[index]
             # Asymmetry
-
+            i_data["Asymmetry"] = asymmetries[index]
             # Eta        
+            i_data["Eta"] = measure_etas_from_xhi(index, trust_adjacency, net.parameters)
+            # Trust histogram
+
+            # bins
 
             # Adding field to dataset data
             self.data[vertices[i].index] = i_data
 
     def init_with_stack(self, stackdir):
-        pass
+        """Initialize Dataset with a Stack"""
+        net = Network()
+        net.reload_with_stack(stackdir)
+        self.init_with_network(net)
 
-    def init_with_hdf5(self, filpath):
-        pass
+    def init_with_hdf5(self, filepath):
+        """Initialize Dataset with a HDF5 file"""
+        net = Network()
+        net.reload_with_hdf5(filepath)
+        self.init_with_network(net)
 
-    def load_from_file(self, filename):
-        pass
-
-    def add_id(self, id, data_dict={}):
-        self.size += 1
+    def add(self, id, data_dict={}):
+        """add field in dataset"""
         if id in self.data:
-            print("WARNING: Erasing previous data")
+            print("WARNING: you are replacing already existing `id` be cautious")
         self.data[id] = data_dict
+        self.size += 1
     
     def add_field_in_id(self, id, key, value):
+        """Add field in the dictionnary stored in id `id`"""
         if not id in self.data:
             self.add_id(id, {key: value})
             return
         self.data[id][key] = value
 
-    def get(self, id, fields="All"):
-        query = Dataset(id)
-        if type(fields) is str:
-            if fields == "All":
-                query.add_id(id, self.data[id])
-                return query
-            if fields in self.data[id]:
-                query.add_field_in_id(id, fields, self.data[id][fields])
-                return query
+    def get_item(self, id, query="All"):
+        """Get item of id `id` in the dataset matching `query` requirment"""
+        output = {}
+        if type(query) is str:
+            if query == "All":
+                output = self.data[id]
+                return output
+            if query in self.data[id]:
+                output[query] = self.data[id][query]
+                return output
             raise KeyError("Unknown field must be in {0}".format(self.data[id].keys()))
         
         try:
-            iterator = iter(fields)
+            iterator = iter(query)
         except TypeError:
-            raise TypeError("`fields` attribute is not iterable and not a `str`")
+            if query == "All":
+                output = self.data[id]
+                return output
+            if query in self.data[id]:
+                output[query] = self.data[id][query]
+                return output
+            raise KeyError("Unknown field must be in {0}".format(self.data[id].keys()))
         
         for field in iterator:
             if field in self.data[id]:
-                query.add_field_in_id(id, field, self.data[id][field])
+                output[field] = self.data[id][field]
             else:
                 raise KeyError("Unknown field must be in {0}".format(self.data[id].keys()))
-        return query
+        return output
+    
+    def get_mutliple_item(self, id_list, query="All"):
+        """Return all entries with ids in `id_list` of the dataset with fields in `query` """
+        output = {}
+        for id in id_list:
+            output[id] = self.get_item(id, query)
 
-    def get_all(self, fields="All"):
-        pass
+    def get_all_item(self, query="All"):
+        """Get all entry of the dataset with fields precised in `query`"""
+        output = {}
+        for id in self.data.keys():
+            output[id] = self.get_item(id, query)
+        return output
 
-    def aggregate(self, fields):
-        query = Dataset("agregated " + self.name + str(fields))
-        for i in range(self.size):
-            data = self.get(i, fields).data
-            for key in data.keys():
-                if key in query.data:
-                    query.data[key][i] = data[key]
+    def aggregate(self, query):
+        """Aggregate the data of same type as in `query` for all the dataset"""
+        dtg = DatasetGroup("aggregated " + str(self.name))
+        data = self.get_all_item(query)
+        for id in self.data.keys():
+            sub_data = data[id]
+            for key, value in sub_data.items():
+                if not dtg.is_in_group(key):
+                    dt = Dataset(key)
+                    dt = dtg.add_dataset(dt)
                 else:
-                    query.data[key] = {i: data[key]}
-        return query
+                    dt = dtg.get_sub(key)
+                dt.add(id, value)
+        return dtg
 
-    def group_by(self, classifier):
-        pass
+    def group_by(self, selector):
+        """Regroup data in dataset by value of the field `Selector` """
+        dtg = DatasetGroup(selector)
+        for i in self.data.keys():
+            if selector in self.data[i]:
+                if not dtg.is_in_group(self.data[i][selector]):
+                    dt = Dataset(self.data[i][selector])
+                    dt = dtg.add_dataset(dt)
+                else:
+                    dt = dtg.get_sub(self.data[i][selector])
+                dt.add(i, self.get_item(i))
+        return dtg
 
     def copy(self):
+        """Copy object"""
         copyds = Dataset(self.name)
-        copyds.data = self.data.copy()
-        copyds.size = self.size
+        for i in self.data.keys():
+            copyds.add(i, self.get_item(i).copy())
         return copyds
-
-    def save(self, filename):
-        pass
+    
+    def keys(self):
+        """Return possible keys of the structure"""
+        return self.data.keys()
 
     def __str__(self):
-        return "Dataset '" + self.name + "' with data: \n" + str(self.data)
+        return "Dataset '" + self.name + "' with data: " + 10 * "-" + "\n" + str(self.data)
 
 
 class DatasetGroup:
 
     def __init__(self, name):
-        self.name = name
+        self.name = str(name)
         self.size = 0
-        self.containt = {}
+        self.subs = {}
     
     def add_dataset(self, dataset):
-        self.containt[dataset.name] = dataset
+        """Add a Dataset object `dataset` in subs"""
+        self.size += 1
+        if dataset.name in self.subs:
+            print("WARNING: Replacing dataset might be a name problem")
+        self.subs[dataset.name] = dataset
+        return dataset
     
-    def get(self, id, selector):
-        pass
+    def add_datasetgroup(self, name):
+        """Add a DatasetGroup object with name `name` in subs"""
+        datagroup = DatasetGroup(name)
+        self.subs[name] = datagroup
+        return datagroup
+    
+    def get_sub(self, name):
+        """Return the sub with name `name` if in subs"""
+        if name in self.subs:
+            return self.subs[name]
+        raise KeyError("The dataset with name {} is not contained in the group".format(name))
 
-    def get_all(self, selector):
-        pass
+    def is_in_group(self, name):
+        """Test is subname `name` is in group"""
+        return name in self.subs
+    
+    def get_item(self, query):
+        """Get item(s) matching `query`
+        if `query` is iterable will return a DatasetGroup with all subs matched"""
+        if type(query) is str:
+            if self.is_in_group(query):
+                return self.get_sub(query)
+            raise KeyError("`query` not valid: not a dataset in the collection")
 
+        try:
+            iterator = iter(query)
+        except TypeError:
+            if self.is_in_group(query):
+                return self.get_sub(query)
+            raise KeyError("`query` not valid: not a dataset in the collection")
+        
+        output = DatasetGroup(self.name + str(query))
+        for field in iterator:
+            if self.is_in_group(field):
+                output.add_dataset(self.get_sub(field).copy())
+            else:
+                raise KeyError("Unknown field")
+        return output
+    
+    def get_all_item(self):
+        """Get all subs that are in the DatasetGroup"""
+        return self
+    
+    def group_by(self, selector):
+        """Group by each content with selector"""
+        copy_dtg = self.copy()
+        for name, content in copy_dtg.subs.items():
+            copy_dtg.subs[name] = content.group_by(selector)
+        return copy_dtg
+    
+    def aggregate(self, query):
+        """Aggregate each content with query"""
+        copy_dtg = self.copy()
+        for name, content in copy_dtg.subs.items():
+            copy_dtg.subs[name] = content.aggregate(query)
+        return copy_dtg
+    
+    def copy(self):
+        """Deep copy the object"""
+        copy_dtg = DatasetGroup(self.name)
+        for name, content in self.subs.items():
+            copy_dtg.subs[name] = content.copy()
+        return copy_dtg
+    
+    def keys(self):
+        """Return possible keys of the DatasetGroup"""
+        return self.subs.keys()
+
+    def __str__(self):
+        string = "DatasetGroup '" + self.name + "' " + 10 * "-" + "\n"
+        for name in self.subs.keys():
+            string += str(name) + "\n"
+            string += str(self.subs[name]) + "\n"
+        return string
